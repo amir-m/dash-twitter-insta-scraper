23fdd097cb2bc2bbdaf3a856ec07f5b18cfae8ce
Dev <amir.moravej@gmail.com>
2014-06-05 00:42:59 -0400

    next

--- master.js
+++ master.js
@@ -7,7 +7,9 @@ var http = require('http'),
 	mongoose = require('mongoose'),
 	cookie = require('cookie'),
 	cluster = require('cluster'),
-	cpuCount = require('os').cpus().length;
+	cpuCount = require('os').cpus().length,
+	app = express(),
+	agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36';
 
 app.set('views', __dirname + '/app');
 app.use(require('body-parser')());
@@ -16,11 +18,37 @@ app.use(require('morgan')('dev'));
 app.set('port', process.env.PORT || 8080);
 
 
-app.listen(app.get('port'), function() {
-	console.log("Listening on " + app.get('port'));
+options = {
+	hostname: 'twitter.com',
+	path: '/m_mozafarian', // TODO: get this from user...
+	headers: {
+		method: 'GET',
+		host: 'twitter.com', 
+		path: '/m_mozafarian',
+		scheme: 'https',
+		version: 'HTTP/1.1',
+		accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'cache-control': 'max-age=0',
+		'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36'
+	}	
+}
+
+
+
+var req = https.request(options, function(res){
+	var data = '';
+
+	res.on('data', function(chunk){
+		data += chunk;
+	});
+
+	res.on('end', function(){
+		console.log(res.headers);
+	});
 });
 
-app.get('/', function(req, res){
-	console.log(res.headers)
-	res.send(200);
-})
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+req.end();
