daaec8c385fb85aa1cf52fbafd459867f14ca5af
Dev <amir.moravej@gmail.com>
2014-06-06 00:17:25 -0400

    twitter done

---
+++ app.js
@@ -0,0 +1,66 @@
+stackTraceLimit = Infinity;
+
+var http = require('http'),
+	https = require('https'),
+	express = require('express'),
+	redis = require('redis'),
+	mongoose = require('mongoose'),
+	cookie = require('cookie'),
+	cluster = require('cluster'),
+	jsdom = require('jsdom'),
+	fs = require('fs'),
+	cpuCount = require('os').cpus().length,
+	app = express(),
+	agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36',
+	workers= {}, left = [], json = [], start;
+	
+
+app.set('views', __dirname + '/app');
+app.use(require('body-parser')());
+app.use(require('method-override')());
+app.use(require('morgan')('dev'));
+app.set('port', process.env.PORT || 8080);
+
+var twitter = require('child_process').fork(__dirname + '/twitter.js');
+
+if (cluster.isMaster) {
+	for (var i = 0; i < cpuCount; i++) {
+		spawn();
+	}
+	cluster.on('exit', function(worker) {
+		console.log('worker ' + worker.id + ' died. spawning a new process...');
+		delete workers[worker.pid];
+		worker.kill();
+		spawn();
+	});
+} else {
+	app.listen(app.get('port'), function() {
+		console.log("Listening on " + app.get('port'));
+	});
+}
+
+function spawn(){
+	var worker = cluster.fork();
+	workers[worker.id] = worker;
+	console.log('worker ' + worker.id + ' was spawned as a new process...');
+	return worker;
+};
+
+app.get('/twitter/:handler', function(req, res){
+	var path;
+	if (req.query.f && req.query.f == 'img') {
+		path = '/' + req.param('handler') +'/media';
+	}
+	else {
+		path = '/' + req.param('handler');	
+	}
+	twitter.send(path);
+	twitter.on('message', function(message){
+		if (message.error) {
+			return res.send(500);
+		}
+		console.log('respond received from twitter...')
+		res.send(message.data);
+	});
+});
+
---
+++ twitter.js
@@ -0,0 +1,123 @@
+stackTraceLimit = Infinity;
+
+console.log('Forking Twitter %s...', process.pid);
+
+var http = require('http'),
+	https = require('https'),
+	redis = require('redis'),
+	mongoose = require('mongoose'),
+	cookie = require('cookie'),
+	cluster = require('cluster'),
+	jsdom = require('jsdom'),
+	fs = require('fs'),
+	cpuCount = require('os').cpus().length,
+	agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36',
+	left = [], json = [], start;
+
+var options = {
+	hostname: 'twitter.com',
+	path: '/', 
+	headers: {
+		accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'cache-control': 'max-age=0',
+		'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36'
+	}	
+};
+
+process.on('message', function(message){
+	console.log('got message')
+	console.log(message);
+	options.path = message;
+	fetch(options);
+});
+
+
+function fetch(options) {
+	console.log('about to fetch')
+	start = new Date().getTime();
+	var req = https.request(options, function(res){
+		var data = '';
+
+		res.on('data', function(chunk){
+			data += chunk;
+		});
+
+		res.on('end', function(){
+			data = data;
+			data = data.replace(/\n/g, '');
+			data = data.replace(/\r/g, '');
+			data = data.replace(/ +(?= )/g,'');
+			data = data.replace(/>\s*</g,'><');
+			
+			var divs = data.toString('utf8').split('<div class="Grid" data-component-term="tweet" role="presentation">');
+			divs.splice(divs.length - 1, 1);
+			divs.splice(0, 1);
+
+			for (var i = 0; i < divs.length; ++i) {
+				divs[i] = '<div class="Grid" data-component-term="tweet" role="presentation">' + divs[i];
+				left.push(i);
+				jsonify(i, divs[i]);
+			};
+
+			console.log('jsonified')
+
+			divs = null;
+			data = null;
+		});
+	});
+	req.on('error', function(e) {
+	  console.log('problem with request: ' + e.message);
+	  process.send({
+	  	error: true
+	  });
+	  global.gc();
+	});
+
+	req.end();
+};
+
+function jsonify(index, div) {
+	console.log('in jsonify')
+	jsdom.env(
+	  div,
+	  ["http://code.jquery.com/jquery.js"],
+	  function (error, window) {
+	  	if (error) {
+	  		process.send({
+	  			error: true
+	  		});
+	  		global.gc();
+	  		throw error;
+	  	}
+	  	console.log('json generated..')
+	  	json.push({
+	  		index: index,
+	  		tweeted_by:  window.$('.ProfileTweet-fullname').text(),
+	  		tweet_body: window.$("p").text(),
+	  		tweet_image_url: window.$('.TwitterPhoto-mediaSource').attr('src'),
+	  		tweet_url: window.$('.twitter-timeline-link').attr('href'),
+	  		retweet_count: window.$('.ProfileTweet-action--retweet .ProfileTweet-actionCountForPresentation').text(),
+	  		favorited_count: window.$('.ProfileTweet-action--favorite .ProfileTweet-actionCountForPresentation').text(),
+	  		tweet_timestamp: window.$('.js-short-timestamp').text()
+	  	});
+	  	left.splice(left.indexOf(index), 1);
+	  	if (left.length == 0) finalize();
+	  	div = null;
+	  }
+	);
+};
+
+function finalize() {
+	json.sort(function(a, b) {
+		return a.index - b.index;
+	});
+
+	for (var i = 0; i < json.length; ++i) {
+		delete json[i].index;
+	};
+	console.log('ready to respond...')
+	process.send({
+		data: json
+	});
+	global.gc();
+};
